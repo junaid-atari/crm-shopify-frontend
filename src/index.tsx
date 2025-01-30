61dced69be82c5e18f5ed3b2b11a50620e889c06
Junaid Atari <junaid.attari@invozone.dev>
2025-01-30 14:02:27 +0500

    use conditional container w or w/o shopify bridge

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+// containers
+import IntentContainer from './elements/containers/IntentApp';
+import PlainContainer from './elements/containers/PlainApp';
+
+// app-core-components
 import App from './App';
 import AppIntent from './AppIntent';
 
-import enTranslations from '@shopify/polaris/locales/en.json';
-import {AppProvider, Page} from '@shopify/polaris';
-
+// styles
 import '@shopify/polaris/build/esm/styles.css';
 
+const isIntentApp = process.env?.REACT_APP_SHOPIFY_INTENT === 'true';
 
-const root = ReactDOM.createRoot(
+var Container = isIntentApp
+  ? PlainContainer
+  : IntentContainer;
+
+ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
-);
-
-root.render(
-  <AppProvider i18n={enTranslations}>
-    <Page>
-      <AppIntent/>
-    </Page>
-  </AppProvider>,
+).render(
+  <Container>
+    {isIntentApp ? <AppIntent/> : <App/>}
+  </Container>,
 );
